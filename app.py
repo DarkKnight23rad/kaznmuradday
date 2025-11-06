0a6160f5ad9c051e9db444a94dceecf35ed7d1c1
DarkKnight23rad <ablai1999@gmail.com>
2025-11-06 11:53:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,14 @@ def quiz():
 
 @app.route("/results", methods=["GET", "POST"])
 def results():
-    if request.method == "POST":
-        data = request.get_json()
-        name = data.get("name")
-        score = data.get("score", 0)
+if request.method == "POST":
+    data = request.get_json()
+    name = data.get("name")
+    score = data.get("score", 0)
 
-        # сохраняем результат
-        leaderboard.append(
+    # сохраняем результат
+    leaderboard.append({"name": name, "score": score})
+    leaderboard.sort(key=lambda x: x["score"], reverse=True)
+
+    return jsonify({"message": "Результаты получены!", "score": score})
+
